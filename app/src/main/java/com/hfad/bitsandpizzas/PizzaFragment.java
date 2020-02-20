9412f2e8cc94575a57d222fe8e0177fb1ec1ba4f
laymanl <laymanl@uncw.edu>
2020-02-20 14:45:16 -0500

    chapter 13 complete

--- app/src/main/java/com/hfad/bitsandpizzas/PizzaFragment.java
+++ app/src/main/java/com/hfad/bitsandpizzas/PizzaFragment.java
@@ -1,38 +1,44 @@
 package com.hfad.bitsandpizzas;
 
-
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.ListFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class PizzaFragment extends ListFragment {
-
-
-    public PizzaFragment() {
-        // Required empty public constructor
-    }
-
-
+public class PizzaFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(
-                inflater.getContext(),
-                android.R.layout.simple_list_item_1,
-                getResources().getStringArray(R.array.pizzas)
-        );
-        setListAdapter(adapter);
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pasta, container, false);
+        RecyclerView pizzaRecycler = (RecyclerView) inflater.inflate(
+                R.layout.fragment_pizza, container, false);
+
+        String[] pizzaNames = new String[Pizza.pizzas.length];
+        for (int i = 0; i < pizzaNames.length; i++) {
+            pizzaNames[i] = Pizza.pizzas[i].getName();
+        }
+
+        int[] pizzaImages = new int[Pizza.pizzas.length];
+        for (int i = 0; i < pizzaImages.length; i++) {
+            pizzaImages[i] = Pizza.pizzas[i].getImageResourceId();
+        }
+        CaptionedImagesAdapter adapter = new CaptionedImagesAdapter(pizzaNames, pizzaImages);
+        pizzaRecycler.setAdapter(adapter);
+
+        GridLayoutManager layoutManager = new GridLayoutManager(getActivity(), 2);
+        pizzaRecycler.setLayoutManager(layoutManager);
+        adapter.setListener(new CaptionedImagesAdapter.Listener() {
+            public void onClick(int position) {
+                Intent intent = new Intent(getActivity(), PizzaDetailActivity.class);
+                intent.putExtra(PizzaDetailActivity.EXTRA_PIZZA_ID, position);
+                getActivity().startActivity(intent);
+            }
+        });
+        return pizzaRecycler;
     }
 }
